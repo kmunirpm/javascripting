7329e9926116445808b3cd5b6a0773d63de233aa
kmunirpm <km092009@gmail.com>
2021-10-09 20:40:51 +0000

    All javascripting exercises~

---
+++ if-statement.js
@@ -0,0 +1,9 @@
+let fruit="orange"
+if(fruit.len>5)
+{
+   console.log("The fruit name has five characters or less.")
+}
+else 
+{
+   console.log("The fruit name has more than five characters.")
+}
